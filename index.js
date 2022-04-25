876b5b5f6cb8241261fd645bf9f99a6899d92400
Alexey Y <alexey.yashenkov@gmail.com>
2022-04-25 10:16:25 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+// @ts-check
+
+import app from './application.js';
+
+const laptops = [
+  {
+    model: 'v1', processor: 'intel', frequency: 1.7, memory: 16,
+  },
+  {
+    model: 'd3', processor: 'intel', frequency: 3.5, memory: 8,
+  },
+  {
+    model: 'd2', processor: 'amd', frequency: 2.5, memory: 16,
+  },
+];
+
+app(laptops);
